f3160322c9bc47e220651c308a31f369029355c6
SantiMolina9 <118134456+SantiMolina9@users.noreply.github.com>
2023-11-28 15:58:24 -0300

    Agregado de package menus 📦️

---
+++ menus/crearTarea.js
@@ -0,0 +1,16 @@
+const Tarea = require('./Tarea');
+const prompt = require("prompt-sync")();
+
+function crearTarea() {
+    const titulo = prompt('Ingrese el título de la tarea: ');
+    const descripcion = prompt('Ingrese la descripción de la tarea: ');
+    const estado = prompt('Ingrese el estado de la tarea: ');
+    const dificultad = prompt('Ingrese la dificultad de la tarea: ');
+    const fechaCreacion = new Date();
+    const fechaVencimiento = prompt('Ingrese la fecha de vencimiento (YYYY-MM-DD): ');
+
+    const nuevaTarea = new Tarea(titulo, descripcion, estado, dificultad, fechaCreacion, new Date(fechaVencimiento));
+    return nuevaTarea;
+}
+
+module.exports = crearTarea;
---
+++ menus/principal.js
@@ -0,0 +1,35 @@
+const readline = require("readline");
+const prompt = require("prompt-sync")();
+const Tarea = require("../Tarea");
+
+let arrayTareas = [];
+
+do{
+console.log("Bienvenido al Menú principal \n");
+console.log("1- Para ver tareas");
+console.log("2- Para crear tarea");
+console.log("3- Para modificar tareas");
+console.log("4- Para borrar tareas");
+console.log("5- Salir");
+    let option = prompt("Ingrese una opcion: ");
+    switch(option){
+        case 1: 
+            //Falta implementar 
+            break;
+        case 2: 
+            const nuevaTarea = crearTarea();
+            arrayTareas.push(nuevaTarea);
+            break;
+        case 3: 
+            //Falta implementar
+            break;
+        case 4:
+            //Falta implementar
+            break;
+        case 5: 
+            console.log("Saliendo...");
+        default:
+            console.log("Ingrese una opcion correcta");
+            break;
+    }
+}while(option != 5);
