dd0188f95e0e3462e318f206f49cc54d72127172
NachoGaynor2003 <128648228+NachoGaynor2003@users.noreply.github.com>
2023-11-28 17:09:14 -0300

    Update menuTareas.js

--- menus/menuTareas.js
+++ menus/menuTareas.js
@@ -1,7 +1,8 @@
-let prompt = require("prompt")
+const prompt = require("prompt-sync")();
 let readline = require("readline")
 console.log(" PRESIONE 1 PARA VER TODAS LAS TAREAs")
 console.log("PRESIONE 2 PARA VER SOLO LAS TAREAS PENDIENTES")
 console.log("PRESIONE 3 PARA VER LAS TAREAS COMPLETADAS")
 console.log("PRESIONE 4 PARA VER LAS TAREAS EN CURSO")
 let opcion = prompt();
+
