c978f9c49ac82723f42d1f38a1af5cce77865cbd
NachoGaynor2003 <nachoteam14@gmail.com>
2023-11-28 17:21:11 -0300

    commit

--- menus/menuTareas.js
+++ menus/menuTareas.js
@@ -1,25 +1,22 @@
 const prompt = require("prompt-sync")();
-let readline = require("readline")
-function tareasMenu(){
-console.log(" PRESIONE 1 PARA VER TODAS LAS TAREAs")
-console.log("PRESIONE 2 PARA VER SOLO LAS TAREAS PENDIENTES")
-console.log("PRESIONE 3 PARA VER LAS TAREAS COMPLETADAS")
-console.log("PRESIONE 4 PARA VER LAS TAREAS EN CURSO")
-let opcion = prompt();
-switch(opcion){
-  case "1": 
-    //falta implementar
-    
-    break;
-  case "2":
-    //falta implementar
-    break;
-  case "3":
-    break;
-  case "4":
-    break;
-    {
-
+let readline = require("readline");
+function tareasMenu() {
+  console.log(" PRESIONE 1 PARA VER TODAS LAS TAREAs");
+  console.log("PRESIONE 2 PARA VER SOLO LAS TAREAS PENDIENTES");
+  console.log("PRESIONE 3 PARA VER LAS TAREAS COMPLETADAS");
+  console.log("PRESIONE 4 PARA VER LAS TAREAS EN CURSO");
+  let opcion = prompt();
+  switch (opcion) {
+    case "1":
+      //falta implementar
+      //dasdsadsa
+      break;
+    case "2":
+      //falta implementar
+      break;
+    case "3":
+      break;
+    case "4":
+      break;
+  }
 }
-
-
